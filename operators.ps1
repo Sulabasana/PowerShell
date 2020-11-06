cf65f393f9b5395f61a378bfbbdb4ba813a0ff60
Sulabasana <piotr.frydryszak@gmail.com>
2020-11-06 20:51:41 +0100

    Something

--- operators.ps1
+++ operators.ps1
@@ -52,7 +52,44 @@ $var %= 2 # Modulus. Divides the value of a variable by the specified value and 
 #Increment and decrement:
 $var++ # Increases the value of a variable, assignable property, or array element by 1
 $var-- # Decreases the value of a variable, assignable property, or array element by 1
+Write-Host "============================"
 
+#DOES NOT WORK
+# #Success output stream:
+# cmdlet > file # Send success output to file, overwriting existing content
+# cmdlet >> file # Send success output to file, appending to existing content
+# cmdlet 1>&2 # Send success and error output to error stream
+# #Error output stream:
+# cmdlet 2> file # Send error output to file, overwriting existing content
+# cmdlet 2>> file # Send error output to file, appending to existing content
+# cmdlet 2>&1 # Send success and error output to success output stream
+# #Warning output stream: (PowerShell 3.0+)
+# cmdlet 3> file # Send warning output to file, overwriting existing content
+# cmdlet 3>> file # Send warning output to file, appending to existing content
+# cmdlet 3>&1 # Send success and warning output to success output stream
+# #Verbose output stream: (PowerShell 3.0+)
+# cmdlet 4> file # Send verbose output to file, overwriting existing content
+# cmdlet 4>> file # Send verbose output to file, appending to existing content
+# cmdlet 4>&1 # Send success and verbose output to success output stream
+# #Debug output stream: (PowerShell 3.0+)
+# cmdlet 5> file # Send debug output to file, overwriting existing content
+# cmdlet 5>> file # Send debug output to file, appending to existing content
+# cmdlet 5>&1 # Send success and debug output to success output stream
+# #Information output stream: (PowerShell 5.0+)
+# cmdlet 6> file # Send information output to file, overwriting existing content
+# cmdlet 6>> file # Send information output to file, appending to existing content
+# cmdlet 6>&1 # Send success and information output to success output stream
+
+
+"The rain in Seattle" -replace 'rain','hail' #Returns: The hail in Seattle
+
+"kenmyer@contoso.com" -replace '^[\w]+@(.+)', '$1' #Returns: contoso.com
+#Split and Join operators:
+# The -split operator splits a string into an array of sub-strings.
+"A B C" -split " " #Returns an array string collection object containing A,B and C.
+
+# The -join operator joins an array of strings into a single string.
+"E","F","G" -join ":" #Returns a single string: E:F:G
 
 
 
